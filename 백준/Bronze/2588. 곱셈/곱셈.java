0928a8cfba210d1b208fa362bc98cb611264d282
032605 <46978277+032605@users.noreply.github.com>
2022-07-16 18:43:48 +0900

    [Bronze III] Title: 곱셈, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -1,19 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
  
-public class Main {
+public class Main{
  
-	public static void main(String[] args) {
-        
-		Scanner sc = new Scanner(System.in);
-		int A = sc.nextInt();
-		String B = sc.next();
-
-		for(int i=2; i>=0; i--) {
-			System.out.println(A * (int) (B.charAt(i)-'0'));
-		}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int A = Integer.parseInt(br.readLine());
+		String B = br.readLine();
 		
-		System.out.println(A * Integer.parseInt(B));
+		br.close();
 		
-		sc.close();
+		System.out.println(A * (B.charAt(2)-'0'));
+		System.out.println(A * (B.charAt(1)-'0'));
+		System.out.println(A * (B.charAt(0)-'0'));
+		System.out.println(A * (Integer.parseInt(B)));
 	}
 }
