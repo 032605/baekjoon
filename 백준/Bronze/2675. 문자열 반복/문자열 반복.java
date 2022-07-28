afb1dbcc30fb4f36353f426c20a7c6ea8b52057f
032605 <46978277+032605@users.noreply.github.com>
2022-07-28 14:54:44 +0900

    [Bronze II] Title: 문자열 반복, Time: 120 ms, Memory: 14040 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,38 +1,31 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main{
 	public static void main(String args[]) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		
 		int T = Integer.parseInt(br.readLine());
 		
-		String[] str = new String[T];
 		int N = 0;
 		
 		for(int i=0; i<T; i++) {
-			String S = br.readLine();
-			
-			StringTokenizer st = new StringTokenizer(S, " ");
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 			
 			N = Integer.parseInt(st.nextToken());	//반복 수
-		
-			str[i] = st.nextToken();
-		
-			for(int j=0; j<str[i].length(); j++) {
+			String str = st.nextToken();			//반복 문자열
+			
+			for(int j=0; j<str.length(); j++) {
 				for(int k=0; k<N; k++) {
-					bw.write(str[i].charAt(j));
+					sb.append(str.charAt(j));
 				}
 			}
-			bw.write("\n");
+			sb.append("\n");
 		}
-		bw.flush();
-		bw.close();
+		System.out.println(sb);
 	}
 }
