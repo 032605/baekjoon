cf659ae9f4abfe8d53ca3f11e897c76d0881f9e5
032605 <46978277+032605@users.noreply.github.com>
2022-08-08 10:31:59 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -4,45 +4,41 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main{
-	public static void main(String[] args) throws IOException  {
+	public static void main(String args[]) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		String input = br.readLine();
-		StringTokenizer st = new StringTokenizer(input, " ");
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-	
-		String num = br.readLine();
-		StringTokenizer st2 = new StringTokenizer(num, " ");
 		
+		StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
 		int[] arr = new int[N];
 		
 		for(int i=0; i<N; i++) {
 			arr[i] = Integer.parseInt(st2.nextToken());
 		}
 		
-		System.out.println(sol(N, M, arr));
-		
-	}
-
-	private static int sol(int N, int M, int[] arr) {
 		int sum = 0;
+		int temp = 0;
 		
 		for(int i=0; i<N-2; i++) {
-			if(arr[i] > M) continue; 
+				if(arr[i]>M) { continue; }
 			for(int j=i+1; j<N-1; j++) {
-				if(arr[i] + arr[j] > M) continue;
+				if(arr[i]+arr[j]>M) { continue; }
 				for(int k=j+1; k<N; k++) {
-					int temp = arr[i] + arr[j] + arr[k];
+					temp = arr[i]+arr[j]+arr[k];
 					
-					if(temp == M) { return M; } 
-					
-					if(temp < M && sum < temp) {
-						sum = temp;
+					if(temp == M) {
+						sum = M;
+						break;
 					}
+					
+					if(temp < M && temp > sum) { sum = temp; }
 				}
 			}
 		}
-		return sum;
+		
+		System.out.println(sum);
 	}
 }
